e9ef40c36a8027ea30ca820a89be6bb8aa5ea394
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2024-01-08 15:28:33 +0530

    logoutBug fix

--- router/auth.Route.ts
+++ router/auth.Route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { LogIn, logOut, socialAuth } from "../controller/userAuth.controller";
+import { isLogIn } from "../middleware/authMiddleware";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", LogIn);
-authRouter.get("/logout", logOut);
+authRouter.get("/logout", isLogIn,logOut);
 
 authRouter.post("/social-auth", socialAuth);
 export default authRouter;
