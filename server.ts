175ecb10a8b948784db357368aa4245fee41edb5
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2024-01-08 19:16:32 +0530

    add http then remove again

--- server.ts
+++ server.ts
@@ -2,8 +2,6 @@ import { app } from "./app";
 import { connectDB } from "./config/db";
 import { cloud_api_key, cloud_api_secret, cloud_name, port } from "./secret/secret";
 import cloudinary from "cloudinary";
-import http from "http";
-const server = http.createServer(app);
 
 cloudinary.v2.config({
     cloud_name: cloud_name,
@@ -11,7 +9,7 @@ cloudinary.v2.config({
     api_secret: cloud_api_secret
 })
 
-server.listen(port,async () => {
+app.listen(port,async () => {
     console.log(`server is running on http://localhost:${port}`);
 
     await connectDB();
