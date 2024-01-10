e11843f1267d32b8fc9d9bc088fa7596f191c403
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2024-01-10 13:57:06 +0530

    add http prtocol and listen the backend server

--- build/server.js
+++ build/server.js
@@ -16,12 +16,14 @@ const app_1 = require("./app");
 const db_1 = require("./config/db");
 const secret_1 = require("./secret/secret");
 const cloudinary_1 = __importDefault(require("cloudinary"));
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer(app_1.app);
 cloudinary_1.default.v2.config({
     cloud_name: secret_1.cloud_name,
     api_key: secret_1.cloud_api_key,
     api_secret: secret_1.cloud_api_secret
 });
-app_1.app.listen(secret_1.port, () => __awaiter(void 0, void 0, void 0, function* () {
+server.listen(secret_1.port, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log(`server is running on http://localhost:${secret_1.port}`);
     yield (0, db_1.connectDB)();
 }));
--- server.ts
+++ server.ts
@@ -2,6 +2,9 @@ import { app } from "./app";
 import { connectDB } from "./config/db";
 import { cloud_api_key, cloud_api_secret, cloud_name, port } from "./secret/secret";
 import cloudinary from "cloudinary";
+import http from "http";
+
+const server = http.createServer(app);
 
 cloudinary.v2.config({
     cloud_name: cloud_name,
@@ -9,7 +12,7 @@ cloudinary.v2.config({
     api_secret: cloud_api_secret
 })
 
-app.listen(port,async () => {
+server.listen(port,async () => {
     console.log(`server is running on http://localhost:${port}`);
 
     await connectDB();
